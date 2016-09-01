648f6a1eb593fae183bd8e58f1be62892af76a10
fabriciorby <fabriciorby@hotmail.com>
2016-09-01 17:04:01 -0300

    commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,32 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.api.inventory.EggIncubator;
+import com.pokegoapi.api.inventory.Inventories;
+import com.pokegoapi.api.inventory.Item;
 import com.pokegoapi.api.inventory.Stats;
 import com.pokegoapi.api.map.Map;
 import com.pokegoapi.api.map.Point;
 import com.pokegoapi.api.map.fort.Pokestop;
+import com.pokegoapi.api.map.fort.PokestopLootResult;
+import com.pokegoapi.api.map.pokemon.CatchResult;
 import com.pokegoapi.api.map.pokemon.CatchablePokemon;
 import com.pokegoapi.util.MapUtil;
 import com.pokegoapi.util.PokeDictionary;
+
+import POGOProtos.Inventory.Item.ItemAwardOuterClass.ItemAward;
+
 import com.pokegoapi.api.player.PlayerProfile;
 import com.pokegoapi.api.player.PlayerProfile.Currency;
+import com.pokegoapi.api.pokemon.EggPokemon;
+import com.pokegoapi.api.pokemon.Pokemon;
 import com.pokegoapi.api.settings.CatchOptions;
 import com.pokegoapi.auth.GoogleUserCredentialProvider;
 import com.pokegoapi.auth.PtcCredentialProvider;
@@ -34,6 +46,8 @@ public class Main {
 		System.out.println("Faça login pelo google (1) ou PTC (2)");
 		Scanner sc = new Scanner(System.in);
 		Integer opcao = sc.nextInt();
+		String username = null, password = null, access = null;
+		GoogleUserCredentialProvider provider;
 		
 		if (opcao == 1) 
 		{
@@ -43,14 +57,14 @@ public class Main {
 			* Afer this, the user must signin on google and get the token that will be show to him.
 			* This token will need to be put as argument to login.
 			*/
-			GoogleUserCredentialProvider provider = new GoogleUserCredentialProvider(httpClient);
+			provider = new GoogleUserCredentialProvider(httpClient);
 
 			// in this url, you will get a code for the google account that is logged
 			System.out.println("Please go to " + GoogleUserCredentialProvider.LOGIN_URL);
 			System.out.println("Enter authorization code:");
 
 			// Ask the user to enter it in the standard input
-			String access = sc.next();
+			access = sc.next();
 
 			// we should be able to login with this token
 			provider.login(access);
@@ -73,17 +87,24 @@ public class Main {
 			* Example log in :
 			*/
 			System.out.println("Digite o usuário: ");
-			String username = sc.next();
+			username = sc.next();
 			
 			System.out.println("Digite a senha: ");
-			String password = sc.next();
+			password = sc.next();
 			
 			go.login(new PtcCredentialProvider(httpClient, username, password));
 		}
 		
+		int contLogin = 0;
 		// After this you can access the api from the PokemonGo instance :
 		PlayerProfile pp = go.getPlayerProfile(); // to get the user profile
 		Stats stats = pp.getStats();
+		Inventories inventories = go.getInventories();
+
+		inventories.getCandyjar();
+
+		inventories.getPokebank();
+		
 		System.out.println("Trainer Name: " + pp.getPlayerData().getUsername());
 		System.out.println("Level: " + stats.getLevel());
 		System.out.println("XP: " + stats.getExperience() + " (" 
@@ -121,14 +142,24 @@ public class Main {
 						pontos.get(0).getLongitude() - parteLong*i,
 						0
 				);
-				System.out.println(go.getLatitude() + ", " + go.getLongitude());
+				//System.out.println(go.getLatitude() + ", " + go.getLongitude());
+				System.out.println(".");
 				Map map = go.getMap();
+				CatchResult cr;
+				PokestopLootResult plr;
 				
 				for (Pokestop pokestop : map.getMapObjects().getPokestops()) // pega todas as pokestops
 				{
 					if (pokestop.canLoot()) 
 					{
-						System.out.println(pokestop.loot().toString());
+						plr = pokestop.loot();
+						System.out.println("Pokestop EXP: " + plr.getExperience());
+						System.out.println("Itens: ");
+						for (ItemAward item :  plr.getItemsAwarded())
+						{
+							System.out.println(item.getItemId());
+						}
+						inventories.updateInventories(); 
 					}
 					
 				}
@@ -137,23 +168,118 @@ public class Main {
 				{
 					pokemon.encounterPokemon();
 					System.out.println("Capturando um " + PokeDictionary.getDisplayName(pokemon.getPokemonIdValue(), Locale.ENGLISH));
-					System.out.println(pokemon.catchPokemon(catchOptions));
+					cr = pokemon.catchPokemon(catchOptions); //add CatchResult
+					if (cr.isFailed())
+					{
+						System.out.println("A captura falhou.");
+					} else {
+						System.out.println("Candies: " + cr.getCandyList().stream().mapToInt(Integer::intValue).sum());
+						System.out.println("XP: " + cr.getXpList().stream().mapToInt(Integer::intValue).sum());
+						System.out.println("Stardust: " + cr.getStardustList().stream().mapToInt(Integer::intValue).sum());
+					}
 					pp.updateProfile();
 					stats = pp.getStats();
+					inventories.updateInventories(); 
+					System.out.println("Status do treinador:");
 					System.out.println("Level: " + stats.getLevel());
 					System.out.println("XP: " + stats.getExperience() + " (" + (stats.getNextLevelXp() - stats.getExperience()) + " to next level)");
-					TimeUnit.SECONDS.sleep(3);
+					sleepRandom(2000, 3000);
+				}
+				
+				for (EggIncubator eggIncubator : inventories.getIncubators()) //Coloca os ovos para chocar
+				{
+					if (!eggIncubator.isInUse())
+					{
+						for(EggPokemon egg : inventories.getHatchery().getEggs())
+						{
+							if (!egg.isIncubate())
+							{
+								egg.incubate(eggIncubator);
+							}
+						}
+					}
+				}
+				
+				for (Item item : inventories.getItemBag().getItems()) //Deleta todas as potions e revives
+				{
+					if ((item.isPotion() || item.isRevive()) && (item.getCount() != 0))
+					{
+						inventories.getItemBag().removeItem(item.getItemId(), item.getCount());
+						inventories.updateInventories(); 
+						sleepRandom(1000, 2000);
+					}
+				}
+				
+				List<Pokemon> pokemonLista = inventories.getPokebank().getPokemons();
+				int totalPokemon = pokemonLista.size();
+				for (int j = 0; j < totalPokemon; j++)
+				{
+					Pokemon pokemon = inventories.getPokebank().getPokemons().get(j);
+					if (
+							pokemon.getPokemonId().equals("PIDGEY") || 
+							pokemon.getPokemonId().equals("WEEDLE") || 
+							pokemon.getPokemonId().equals("CATERPIE")
+						)
+					{
+						if (pokemon.canEvolve())
+						{
+							System.out.println("Evoluindo: " + pokemon.getPokemonId());
+							System.out.println(pokemon.evolve());
+							sleepRandom(1000, 2000);
+						}
+					}
+					if (pokemon.getIvRatio() < 0.8)
+					{
+						System.out.println("Transferindo: " + pokemon.getPokemonId());
+						System.out.println(pokemon.transferPokemon());
+						inventories.updateInventories(true);
+						totalPokemon--;
+						sleepRandom(1000, 2000);
+					} 
+					else
+					{
+						if (pokemon.canEvolve())
+						{
+							System.out.println("Evoluindo: " + pokemon.getPokemonId());
+							System.out.println(pokemon.evolve());
+							sleepRandom(1000, 2000);
+						}
+					}
 				}
 
-				TimeUnit.SECONDS.sleep(1);
+				contLogin++;
+				if (contLogin == 500) //reseta a autorização a cada 10 minutos +/-
+				{
+					if (opcao == 1)
+					{
+						provider = new GoogleUserCredentialProvider(httpClient);
+						provider.login(access);
+						go.login(provider);
+					} else {
+						go.login(new PtcCredentialProvider(httpClient, username, password));
+					}
+					contLogin = 0;
+				}
+				sleepRandom(1000, 2000);
 			}
+			
+			
 			
 		}
 
-		
-		
-		//go.getInventories(); // to get all his inventories (Pokemon, backpack, egg, incubator)
 	}
 	
+	public static void sleepRandom(int minMilliseconds, int maxMilliseconds) {
+		Random random = new Random(System.currentTimeMillis());
+        int from = Math.max(minMilliseconds, maxMilliseconds);
+        int to = Math.min(minMilliseconds, maxMilliseconds);
+        try {
+            int randomInt = random.nextInt((from - to) + 1) + to;
+            System.out.println("Esperando " + (randomInt / 1000.0F) + " segundos.");
+            TimeUnit.MILLISECONDS.sleep(randomInt);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 		
 }
